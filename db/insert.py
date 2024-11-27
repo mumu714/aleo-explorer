9f6d3b2f67cc28364690a6d2df8e5202e40940a8
mumu741 <panlin@web3.com>
2024-11-27 18:09:25 +0800

    ARC42

--- db/insert.py
+++ db/insert.py
@@ -1312,7 +1312,11 @@ class DatabaseInsert(DatabaseBase):
 
                     try:
                         if block.height != 0:
+                            from db import Database
+                            last_block_timestamp = await cast(Database, self).get_latest_block_timestamp()
+                            time_since_last_block = block.header.metadata.timestamp - last_block_timestamp
                             block_reward, coinbase_reward = block.compute_rewards(
+                                time_since_last_block,
                                 await cast("Database", self).get_latest_coinbase_target(),
                                 await cast("Database", self).get_latest_cumulative_proof_target()
                             )
--- node/mainnet/param.py
+++ node/mainnet/param.py
@@ -31,3 +31,5 @@ class Mainnet:
     ans_registry = "aleo_name_service_registry.aleo"
 
     restrictions_id = Field(7562506206353711030068167991213732850758501012603348777370400520506564970105)
+
+    consensus_v2_height = 2800000
